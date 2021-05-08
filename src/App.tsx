703e55473492f6d709d7ebcdd197fa2741457064
Fran Plaza <franplazacodes@gmail.com>
2021-05-08 03:17:24 +0200

    Create component to pick images

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import cat1 from './images/cat1.png';
+import cat2 from './images/cat2.png';
+import cat3 from './images/cat3.png';
+
+interface catImg {
+  id: number,
+  location: string,
+}
+
+const catImages: catImg[] = [
+  { id: 0, location: cat1 },
+  { id: 1, location: cat2 },
+  { id: 2, location: cat3 }
+];
 
 function App() {
+  const [currentImg, setCurrentImg] = React.useState<catImg>(catImages[0]);
+
+  function changePicture() {
+    const nextIndex = currentImg.id + 1 === catImages.length ? 0 : currentImg.id + 1;
+    setCurrentImg(catImages[nextIndex]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={changePicture}>CHANGE THE CAT!!! 🙀🙀🙀</button>
+      <img
+        src={currentImg.location}
+        alt='Cat looking at the camera'
+      ></img>
     </div>
   );
 }
